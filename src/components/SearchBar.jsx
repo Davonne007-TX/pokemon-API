d59cd3b94a9f8a249bca8cf2633e36401eee1bed
Vonney <davonnejv@gmail.com>
2023-10-08 11:06:33 -0500

    added a function to handle searching for a pokemon

--- src/components/SearchBar.jsx
+++ src/components/SearchBar.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 import "./css/SearchBar.css";
 
-export function SearchBar() {
+export function SearchBar({ value, setSearchedPokemon }) {
+  const handleSearch = (e) => {
+    const inputSearch = e.target.value;
+    setSearchedPokemon(inputSearch);
+  };
   return (
     <div className="searchBar">
-      <input type="search" placeholder="Search For Pokemon" />
+      <input
+        type="search"
+        placeholder="Search For Pokemon"
+        onChange={handleSearch}
+        value={value}
+      />
     </div>
   );
 }
