59eb1fef4850caefada232d67e7933534b122288
Vonney <davonnejv@gmail.com>
2023-10-08 10:24:19 -0500

    added styles to search bar and Pokemon container

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,10 +1,11 @@
 import React from "react";
 import { SearchBar } from "./SearchBar";
+import "./css/Pokemon.css";
 
 export function Pokemon() {
   return (
-    <>
+    <div className="pokemon-container">
       <SearchBar />
-    </>
+    </div>
   );
 }
--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 
 export function Profile() {
-  return <div>Profile</div>;
+  return (
+    <div>
+      <h1 style={{ color: "black", marginTop: "200px" }}>Welcome ..!</h1>
+    </div>
+  );
 }
