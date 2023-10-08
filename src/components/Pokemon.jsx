6c0bebda93decf5eb74fc9a9b37c3ab494f6ceac
Vonney <davonnejv@gmail.com>
2023-10-07 23:46:26 -0500

    working on my searchBar.jsx

--- src/components/Pokemon.jsx
+++ src/components/Pokemon.jsx
@@ -1,9 +1,10 @@
 import React from "react";
+import { SearchBar } from "./SearchBar";
 
 export function Pokemon() {
   return (
     <>
-      <div>Pokemon</div>
+      <SearchBar />
     </>
   );
 }
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+import "./css/SearchBar.css";
+
+export function SearchBar() {
+  return (
+    <div className="searchBar">
+      <input type="search" placeholder="Search For Pokemon" />
+    </div>
+  );
+}
