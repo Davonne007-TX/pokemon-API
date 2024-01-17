aa5ae91a62f17397ff1ae4ce72500c85c68f1293
Vonney <davonnejv@gmail.com>
2024-01-16 20:44:33 -0600

    styling single selected pokemon

--- src/components/SinglePokemon.jsx
+++ src/components/SinglePokemon.jsx
@@ -1,8 +1,15 @@
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export default function SinglePokemon({ pokemon }) {
   const [singlePokemon, setSinglePokemon] = useState("");
 
+  //useNavigate
+  const navigate = useNavigate();
+
+  const backToPokedex = () => {
+    navigate("/pokemon");
+  };
   useEffect(() => {
     console.log("Selected Pokemon:", pokemon);
     if (pokemon) {
@@ -15,9 +22,17 @@ export default function SinglePokemon({ pokemon }) {
   }
   return (
     <>
-      <div>
-        <h2>{singlePokemon.name}</h2>
-        <img src={singlePokemon.image} alt="Pokemon Selected Image" />
+      <div className="flex justify-center items-center">
+        <div className="flex flex-col justify-center items-center mt-40 bg-bright w-96 h-56">
+          <h2 className="text-2xl">{singlePokemon.name}</h2>
+          <img src={singlePokemon.image} alt="Pokemon Selected Image" />
+          <button
+            className="hover:text-blueBright cursor-pointer font-mono"
+            onClick={backToPokedex}
+          >
+            Back To Pokedex
+          </button>
+        </div>
       </div>
     </>
   );
