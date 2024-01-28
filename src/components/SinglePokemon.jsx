e33159fcb0fba9147c4d74e4f292638f3bb6c753
Vonney <davonnejv@gmail.com>
2024-01-28 16:18:19 -0600

    adding animation to backImage and adding font to tailwind config

--- src/components/SinglePokemon.jsx
+++ src/components/SinglePokemon.jsx
@@ -26,19 +26,21 @@ export default function SinglePokemon({ pokemon }) {
         <div className="bg-bright w-full m-5 md:2/12 lg:w-4/12 rounded border-8 border-blueBright">
           <div className="flex flex-col ml-5 mt-5 mb-5 font-mono">
             <div className="flex flex-row">
-              <h2 className="text-2xl mb-10">{singlePokemon.name}</h2>
+              <h2 className="text-4xl mb-10">{singlePokemon.name}</h2>
               <div className="ml-auto">
                 <img
                   src={singlePokemon.backImage}
                   alt="Pokemon Selected Image"
-                  className=" max-w-full h-auto ml-auto mr-auto"
+                  className=" max-w-full h-auto ml-auto mr-auto animate-360"
                 />
               </div>
             </div>
 
-            <p>Height:{singlePokemon.height}</p>
-            <p>Weight:{singlePokemon.weight}</p>
-            <p>Base Experience: {singlePokemon.base_experience}</p>
+            <div className="font-orb text-2xl">
+              <p>Height:{singlePokemon.height}</p>
+              <p>Weight:{singlePokemon.weight}</p>
+              <p>Base Experience: {singlePokemon.base_experience}</p>
+            </div>
             <div className="flex flex-row ">
               <img
                 src={singlePokemon.image}
